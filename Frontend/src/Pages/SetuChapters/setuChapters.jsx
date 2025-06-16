205424ed78e6d28b3daf189692cd1e70c3bae0ec
Srehansu@123 <srehansu@roalytics.com>
2025-06-16 16:20:30 +0530

    updated chap

--- Frontend/src/Pages/SetuChapters/setuChapters.jsx
+++ Frontend/src/Pages/SetuChapters/setuChapters.jsx
@@ -28,7 +28,7 @@ const chaptersEnglish = [
   { title: "Chapter 23: The immortal Lord Hanuman talks about the marriage of Lord Ram and Devi Sita", link: "https://drive.google.com/file/d/1iwmB0wGx-7oMW6JCJHR_lmyAwNTVvf1w/view?usp=drive_link" },
   { title: "Chapter 24: Battle against Maya – Knowledge of Karma, Destiny, and Freewill", link: "https://drive.google.com/file/d/1kRc1MyDUSo2o3CoC5acRZ8uPSPzagHju/view?usp=drive_link" },
   { title: "Chapter 25: Story of Ahalya told by the immortal Lord Hanuman", link: "https://drive.google.com/file/d/1c3rb8BO9mbM50jaN3W0kVCzYoKAPE4LA/view?usp=drive_link" },
-  { title: "Chapter 26: The immortal Lord Hanuman talks about the marriage of Lord Ram and Devi Sita", link: "https://drive.google.com/file/d/1_L37ToFBEzEQSKkuesoGfOUwUiAo6gZS/view?usp=drive_link" },
+  { title: "Chapter 26: The immortal Lord Hanuman talks about the marriage of Lord Ram and Devi Sita", link: "https://drive.google.com/file/d/1TnNvtyAKlSmIpNXIxJGWkgm3WYfB9fBO/view?usp=drive_link" },
   { title: "Chapter 27: Battle against Maya – Knowledge of Karma, Destiny, and Freewill", link: "https://drive.google.com/file/d/1_L37ToFBEzEQSKkuesoGfOUwUiAo6gZS/view?usp=drive_link" },
 ];
 const chaptersHindi = [
