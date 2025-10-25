920f4d4eaa599ef0c8d24589b83a817e40f19e96
SetuSouls03 <setusouls@gmail.com>
2025-10-25 12:38:42 +0530

    Update authControllersss.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -16,9 +16,6 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false, // 👈 This bypasses the certificate chain check
-  },
 });
 
 // Helper: get client IP
